6c29c279165900ab945b78af4b6a60452e6d03cf
mathuvu <mvideau@meta.com>
2025-01-27 16:45:57 +0000

    adding tiedlinear to fastRNNs and Mamba

--- apps/mamba/mamba.py
+++ apps/mamba/mamba.py
@@ -6,7 +6,7 @@ from typing import Optional
 import torch
 from torch import nn
 
-from lingua.transformer import RMSNorm, cross_entropy
+from lingua.transformer import RMSNorm, TiedLinear, cross_entropy
 from apps.mamba.core_mamba import BaseMambaArgs, BaseMamba
 
 
@@ -73,14 +73,14 @@ class LMMamba(BaseMamba):
 
         self.norm = RMSNorm(args.dim, eps=args.norm_eps)
 
-        self.output = nn.Linear(
-            args.dim,
-            args.vocab_size,
-            bias=False,
-        )
-
         if args.weight_tying:
-            self.output.weight = self.embeddings.tok_embeddings.weight
+            self.output = TiedLinear(self.tok_embeddings)
+        else:
+            self.output = nn.Linear(
+                args.dim,
+                args.vocab_size,
+                bias=False,
+            )
 
     def forward(
         self,
